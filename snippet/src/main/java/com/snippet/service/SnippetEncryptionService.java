a8d8feb80ac68060edcdf60702d124edb51937da
ktinson <monikai.tinson@verizon.com>
2024-11-19 07:37:15 -0600

    added division

--- snippet/src/main/java/com/snippet/service/SnippetEncryptionService.java
+++ snippet/src/main/java/com/snippet/service/SnippetEncryptionService.java
@@ -5,18 +5,21 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SnippetEncryptionService {
 
-    private static final String ALGORITHM = "AES";
-    private static final String SECRET_KEY = "1234567890123456"; // 16-byte key (AES-128)
-
+  // 16-byte key (AES-128)
+  private static final String ALGORITHM = "AES";
+  @Value("$SNIPPET_SECRET_KEY")
+  private String secretKey;
     // Encrypt the snippet code
     public String encrypt(String code) throws Exception {
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+        SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, keySpec);
         byte[] encrypted = cipher.doFinal(code.getBytes());
         return Base64.getEncoder().encodeToString(encrypted);
@@ -25,7 +28,7 @@ public class SnippetEncryptionService {
     // Decrypt the snippet code
     public String decrypt(String encryptedCode) throws Exception {
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+        SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, keySpec);
         byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(encryptedCode));
         return new String(decrypted);
